d4d1a5193617b8881becc0daafdea3be8576d557
KenleyChiu <kenley_chiu@dlsu.edu.ph>
2020-09-24 01:08:05 +0800

    modified community;

--- community.php
+++ community.php
@@ -92,6 +92,56 @@
 		</div>
 				
 		<div class ="post">
+		<?php		
+					$postsArr = array();
+					$community=$_SESSION['commSelected'];
+					$postsQuery = mysqli_query($data,"select * from posts where community = '$community'");
+					
+					$postArr = array();
+					
+					
+					while($posts = mysqli_fetch_array($postsQuery)){
+						$postsArr["Author"] = $posts["Author"];
+						$postsArr["Title"] = $posts["Title"];
+						$postsArr["TextContent"] = $posts["TextContent"];
+						$postsArr["ImageContent"] = $posts["ImageContent"];
+						$postsArr["Likes"] = $posts["Likes"];
+						$postsArr["Comments"] = $posts["Comments"];
+						$postsArr["Community"] = $posts["Community"];
+						$postsArr["PostType"] = $posts["PostType"];
+						$postsArr["Upload"] = $posts["Upload"];
+						array_push($postsArr,$postsArr["Author"],$postsArr["Title"],$postsArr["TextContent"],$postsArr["ImageContent"],$postsArr["Likes"],$postsArr["Comments"]
+						,$postsArr["Community"],$postsArr["PostType"],$postsArr["Upload"]);
+						array_push($postArr,$postsArr);
+						
+						echo "<div class='singlePost'>";
+						echo "<a href='users.php'><img src='pictures/user.png'></a>";
+						echo "<label class='postUser'><a class='postUser' href='users.php' > ".$postsArr["Author"]." </a></label><br><br>";
+						echo "<form method='post'>";
+						echo "<input class='postTitleBtn' type='submit' name='".$postsArr["Title"]."' value='".$postsArr["Title"]."'/><br><br>";
+						echo "</form>";
+						//echo "<label class='postTitle'><a class='postTitle' href='' name='".$postsArr["Title"]."'> ".$postsArr["Title"]." </a></label><br>";
+						echo "<label class='stars'> ".$postsArr["Likes"]." Stars </label>";
+						echo "<label class='comments'> ".$postsArr["Comments"]." Comments </label>";
+						echo "<br>";
+						echo "</div>";
+					}
+					
+					foreach($postArr as $post){
+						if(isset($_POST[$post["Title"]])){
+							$_SESSION['statusPost'] = "selected";
+							$_SESSION['AuthorPost'] = $post["Author"];
+							$_SESSION['TitlePost'] = $post["Title"];
+							$_SESSION['ImageContentPost'] = $postsArr["ImageContent"];
+							$_SESSION['TextContentPost'] = $post["TextContent"];
+							$_SESSION['StarsPost'] = $post["Likes"];
+							$_SESSION['CommentsPost'] = $post["Comments"];
+							$_SESSION['PostTypePost'] = $post["PostType"];
+							header("Location: post.php");
+						}
+					}
+					mysqli_close($data);
+				?>
 		
 		</div>
 			
