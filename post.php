22af2c3b7bfbd69e2c5ce5139266507efd36db28
KenleyChiu <kenley_chiu@dlsu.edu.ph>
2020-09-24 17:32:41 +0800

    comment counter working;

--- post.php
+++ post.php
@@ -32,7 +32,7 @@
 					$postTitle = $_SESSION['TitlePost'];
 					$postImageContent = $_SESSION['ImageContentPost'];
 					$postPostType = $_SESSION['PostTypePost'];
-					$postComment = $_SESSION['CommentsPost'];
+					$commentsCount = $_SESSION['CommentsPost'];
 					
 					$errorMessage = " ";
 					
@@ -46,11 +46,68 @@
 								$likesCount = $_SESSION['StarsPost'] + 1;
 								$addLikes = "update posts set Likes='".$likesCount."' where Title='".$postTitle."'";
 								mysqli_query($data,$addLikes);
+								$_SESSION['StarsPost']=$likesCount;
 								
 								$addLikes2 = "insert into postlikes values('".$userarray[0]."','".$postTitle."','".$community."','".$postPostType."')";
 								mysqli_query($data,$addLikes2);
 							}
 						} else {
+							$errorMessage = "You have to be logged in to do that!";
+						}
+					}
+					// if create comment button is pressed
+					if(isset($_POST['commentBtn'])){
+						if($signedInStatus == "True"){		
+							$comment="";
+							if ($_SERVER["REQUEST_METHOD"] == "POST")
+							{
+								if(empty($_POST['commentContent']))
+								{
+									$errorMessage = "Fill up Comment";
+								}
+								else{
+									$comment= $_POST["commentContent"];
+									if(empty($_FILES["commentImage"]["name"]))
+									{	
+										$statement="Insert into comments(Username,Title,TextComment,Likes,Community,PostType,Upload) values ('$userarray[0]','$postTitle','$comment','0','$community','Thread',NOW())";
+										mysqli_query($data,$statement);
+										$commentsCount++;
+										$addComment = "update posts set Comments='".$commentsCount."' where Title='".$postTitle."'";
+										mysqli_query($data,$addComment);
+										$_SESSION['CommentsPost']=$commentsCount;
+									}else{
+											//get file info
+										$fileName = $_FILES["commentImage"]["name"];
+										$fileError= $_FILES["commentImage"]["error"];
+										$filetmp = $_FILES["commentImage"]["tmp_name"];
+										$fileExt = explode('.',$fileName);
+										$fileActualExt = strtolower(end($fileExt));
+										// Allow certain file formats 
+										$allowTypes = array('jpg','png','jpeg','gif'); 
+										if(in_array($fileActualExt, $allowTypes)){ 
+											if($fileError === 0){
+												$fileNameNew = $fileExt[0].".".$fileActualExt;
+												$fileDestination ='upload/'.$fileNameNew;
+												move_uploaded_file($filetmp,$fileDestination);
+												$statement = "Insert into comments values ('$userarray[0]','$postTitle','$comment','$fileDestination','0','$community','Thread',NOW())";
+												$status=mysqli_query($data,$statement);
+												if($status){
+													$errorMessage="File sucessfully Upload";
+													$commentsCount++;
+													$addComment = "update posts set Comments='".$commentsCount."' where Title='".$postTitle."'";
+													mysqli_query($data,$addComment);
+													$_SESSION['CommentsPost']=$commentsCount;
+												}else{
+													$errorMessage="File upload Failed";
+												}
+											}else{
+												$errorMessage = "Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.";
+											}
+										}
+									}
+								}
+							}
+						 }else {
 							$errorMessage = "You have to be logged in to do that!";
 						}
 					}
@@ -64,8 +121,10 @@
 						echo "<form class='starsForm' action='".$_SERVER['PHP_SELF']."' method='post'>";
 						echo "<input class='likeBtn' type='submit' name='likeBtn' value='Star'></form>";
 						echo "<label class='stars'>" .$likesCount." Stars </label>";
-						echo "<label class='comments'>" .$postComment." Comments </label>";
+						echo "<label class='comments'>" .$commentsCount." Comments </label>";
 					}
+
+					
 				?>
 				<!--only if post has image/s
 					<img src="pictures/user.png"><br>
@@ -74,64 +133,7 @@
 			</div>
 			
 			<?php 				
-				if(isset($_POST['commentBtn'])){
-					if($signedInStatus == "True"){		
-						$comment="";
-						$errorMessage="";
-						if ($_SERVER["REQUEST_METHOD"] == "POST")
-						{
-							if(empty($_POST['commentContent']))
-							{
-								$errorMessage = "Fill up Comment";
-							}
-							else{
-								$comment= $_POST["commentContent"];
-								if(empty($_FILES["commentImage"]["name"]))
-								{	
-									$statement="Insert into comments(Username,TextComment,Likes,Community,PostType,Upload) values ('$userarray[0]','$comment','0','$community','Thread',NOW())";
-									mysqli_query($data,$statement);
-								}else{
-										//get file info
-									$fileName = $_FILES["commentImage"]["name"];
-									$fileError= $_FILES["commentImage"]["error"];
-									$filetmp = $_FILES["commentImage"]["tmp_name"];
-									$fileExt = explode('.',$fileName);
-									$fileActualExt = strtolower(end($fileExt));
-									// Allow certain file formats 
-									$allowTypes = array('jpg','png','jpeg','gif'); 
-									if(in_array($fileActualExt, $allowTypes)){ 
-										if($fileError === 0){
-											$fileNameNew = $fileExt[0].".".$fileActualExt;
-											$fileDestination ='upload/'.$fileNameNew;
-											move_uploaded_file($filetmp,$fileDestination);
-											$statement = "Insert into comments values ('$userarray[0]','$comment','$fileDestination','0','$community','Thread',NOW())";
-											$status=mysqli_query($data,$statement);
-											if($status){
-												$errorMessage="File sucessfully Upload";
-											}else{
-												$errorMessage="File upload Failed";
-											}
-										}else{
-											$errorMessage = "Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.";
-										}
-									}
-								}
-							}
-						}
-						// if(!empty($_POST['commentContent'])){
-						// 	$commentsCount = $_SESSION['CommentsPost'] + 1;
-						// 	echo $commentsCount;
-						// 	$addComment = "update posts set Comments='".$commentsCount."' where Title='".$postTitle."'";
-						// 	mysqli_query($data,$addComment);
-							
-						// 	$addComment2 = "insert into comments values('".$userarray[0]."','".$postTitle."','".$_POST['commentContent']."','','0','".$community."','".$postPostType."')";
-						// 	//".$_POST['commentImage']."
-						// 	mysqli_query($data,$addComment2);
-						// }
-				 	}else {
-						$errorMessage = "You have to be logged in to do that!";
-					}
-				}
+				
 			?>
 			
 			<div class="comment">
